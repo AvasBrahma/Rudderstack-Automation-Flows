680dcafd797c085a40e532c3ee1b43fe1e929ec7
AvasBrahma <avasbrahma@gmail.com>
2025-08-12 02:07:35 +0530

    RudderStack Steps added

---
+++ step-definitions/rudderStackSteps.js
@@ -0,0 +1,18 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const RudderStackPageObject = require('../pageobjects/rudderStackPageObject');
+
+
+When('Read {string} and Save as {string} from the Rudder Stack Dashboards', async (propertyName, keyName) => {
+    let rudderStack = new RudderStackPageObject(browser);
+    if(propertyName=='Data Plane'){
+      await rudderStack.readDataPlaneAndSave(keyName);
+    }else if(propertyName=='Write Key'){
+      await rudderStack.readWriteKeyAndSave(keyName);
+    }
+});
+
+Then('Send a HTTP POST API request to {string} with {string} payload and Verify Response Code', async (endPointName, payloadName) => {
+    let rudderStack = new RudderStackPageObject(browser);
+    await rudderStack.sendHTTPPostRequest(endPointName, payloadName);
+
+});
