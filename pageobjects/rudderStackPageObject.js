a8844fcd22f40b208f7a2dca99723e1e69129694
AvasBrahma <avasbrahma@gmail.com>
2025-08-14 00:01:56 +0530

    optimise rudderstack common object functions

--- pageobjects/rudderStackPageObject.js
+++ pageobjects/rudderStackPageObject.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const { WDioHelper }=require('../utils/wdioHelper');
 const { saveTestData, getTestData}=require('../utils/dynamicDataHelper');
 const { APIHelper } = require('../utils/apiHelper');
-const assert = require('assert')
+const assert = require('assert');
+const { logger } = require("../utils/loggerHelper");
 
 class RudderStackPageObject {
     constructor(browserInstance) {
@@ -17,49 +18,52 @@ class RudderStackPageObject {
          this.tableAllRowsColumnDataXPath="//tbody/tr/td[@class='ant-table-cell'][{columnNumber}]/span";
     }
 
-    async readWriteKeyAndSave(keyName){
+    async saveWriteKey(keyName){
          try {
-            const writeKeyEle =  await this.browser.$(this.writeKeySpanXpath);
-             let writeKeyText = await writeKeyEle.getText();
+             let writeKeyText= await WDioHelper.getText(this.writeKeySpanXpath);
+             if(writeKeyText.trim()!=""&&writeKeyText!=null){
              writeKeyText = writeKeyText.split(" ")[2];
              await saveTestData(keyName, writeKeyText);
              await WDioHelper.takeScreenshot("WriteKey");
-    } catch (error) {
-        console.log("Error while trying to read write key");
-    }
-
-    }
-
-     async readDataPlaneAndSave(keyName){
-         try {
-        await this.browser.waitUntil(
-            async () => {
-                const el = await this.browser.$(this.askAIBetaPopUp);
-                return await el.isDisplayed();
-            },
-            {
-                timeout: 15000,
-                timeoutMsg: `Popup icon not visible within 15s`
-            }
-        )
-        const popupIconEl = await this.browser.$(this.askAIBetaPopUp);
-        if (await popupIconEl.isDisplayed()) {
-             await popupIconEl.click();
-             const dataPlaneSpanEle =  await this.browser.$(this.dataPlaneSpanXpath);
-             let dataPlaneText = await dataPlaneSpanEle.getText();
-             await saveTestData(keyName, dataPlaneText);
-             await WDioHelper.takeScreenshot("DataPlane");
-        } else {
-             console.log("Popup not visible");
+             logger.info(`User Successfully Save Write Key`);
+             }else{
+            logger.error(`Got Write Key as Null or Blank, WriteKey: ${writeKeyText}`);
         }
-
+            
     } catch (error) {
-        console.log("Popup not found within 15s, skipping...");
+        throw new Error(`Error while trying to save write key, ${error.message}`);
     }
 
+    }
+
+    async handlingRudderStackUserSaveAction(elementName, keyName){
+        try {
+        let textLocator="";
+        if(elementName=="Data Plane"){
+            textLocator=this.dataPlaneSpanXpath;
+            await WDioHelper.waitForElementVisible(this.askAIBetaPopUp, "AskAIBetaPopUp");
+            const popupIconEl = await this.browser.$(this.askAIBetaPopUp);
+           if(await popupIconEl.isDisplayed()) {
+             await popupIconEl.click();
+          }
+        }else if(elementName=="Delivered"){
+            textLocator=this.desEventCountXpath.replace("{EventName}","Delivered");
+            await WDioHelper.waitForElementVisible(textLocator, "DeliveryCount");
+        }else if(elementName=="Failed"){
+             textLocator=this.desEventCountXpath.replace("{EventName}",'Failed');
+        }
+        let textFound=await WDioHelper.getText(textLocator);
+        await saveTestData(keyName, textFound);
+        await WDioHelper.takeScreenshot(elementName);   
+        logger.info(`User Saving ${elementName} value as key ${keyName}`);    
+        } catch (error) {
+          throw new Error(`Error while trying to read and Save ${elementName}, ${error.message}`);
+        }
     }
 
 async sendHTTPPostRequest(endpointName, payloadName){
+    try {
+    logger.info(`User Sending HTTP POST Request in an Endpoint: ${endpointName}`);
     let baseURL=await getTestData("DataPlane");
     let basicAuthValue=await getTestData("WriteKey");
     const username = basicAuthValue;
@@ -71,67 +75,30 @@ async sendHTTPPostRequest(endpointName, payloadName){
     await APIHelper.addHeaders('Authorization',"Basic "+encodedCredentials);
     await APIHelper.setPayload(payloadName);
     await APIHelper.sendPOSTReq();
-    let response=await APIHelper.getResponse();
-    assert.strictEqual(response.status, 200, 
-        `Expected 200 but got ${response.status}`
-    );
-  console.log('Response status is 200 as expected');
-
+    await APIHelper.verifyResponseCode(200);
+    } catch (error) {
+    throw new Error(`Error while trying to Send HTTP POST Request in Endpoint ${endpointName}, ${error.message}`);   
+    }
 }
 
 
-async goToWebHooksAndClickTab(webHookName, tabName){
+async navigateToTabUnderDestination(webHookName, tabName){
     let webHookDesXpath=this.webHookDestinationButton.replace("{destinationName}", webHookName);
-    const webHookDesEle = await this.browser.$(webHookDesXpath);
-    await webHookDesEle.click();
+    await WDioHelper.clickAction(webHookDesXpath,webHookName,"Desination Board");
     await WDioHelper.takeScreenshot(webHookName);
-    
-    let webHookDesTabXpath=this.webHookDestinationSubTab.replace("{tabName}", tabName); await this.browser.waitUntil(
-    async () => await (await this.browser.$(webHookDesTabXpath)).isDisplayed(),
-        {
-            timeout: 15000,
-            timeoutMsg: `Destination WebHook Tab Button ${tabName} was not visible within 15s`
-        });
-    const webHookDesTabEle = await this.browser.$(webHookDesTabXpath);
-    await webHookDesTabEle.click();
+    logger.info(`User Navigating Inside Destination ${webHookName}`);
+    let webHookDesTabXpath=this.webHookDestinationSubTab.replace("{tabName}", tabName); 
+    await WDioHelper.waitForElementVisible(webHookDesTabXpath,tabName);
+    await WDioHelper.clickAction(webHookDesTabXpath,tabName,"Tab");
     const newWebHookDesTabEle = await this.browser.$(webHookDesTabXpath);
     const isTabClick = await newWebHookDesTabEle.getAttribute('aria-selected');
     if(isTabClick=="true"){
-        console.log(`Web Hook Destination Tab ${tabName} click successfully`);
+        logger.info(`Web Hook Destination Tab ${tabName} click successfully`);
         await WDioHelper.takeScreenshot(tabName);
     }else{
         console.log(`Issue while clicking Web Hook Destination Tab ${tabName}`);
     }
-    
 
-}
-
-async readAndSaveDeliveredCounts(){
-    let destinationDeliveryCountSpanXpath=this.desEventCountXpath.replace("{EventName}","Delivered");
-     await this.browser.waitUntil(
-            async () => {
-                const el = await this.browser.$(destinationDeliveryCountSpanXpath);
-                return await el.isDisplayed();
-            },
-            {
-                timeout: 15000,
-                timeoutMsg:`Delivery Count is not visible within 15s`
-            }
-        )
-    const desDelCountEle = await this.browser.$(destinationDeliveryCountSpanXpath);
-    let deliveredCount=await desDelCountEle.getText();
-    console.log("Delivered Count : ", deliveredCount);
-    await saveTestData("DeliveryCount", deliveredCount);
-
-}
-
-async readAndSaveFailedEvents(){
-    let destinationFailedCountSpanXpath=this.desEventCountXpath.replace("{EventName}",'Failed');
-    const desFailedCountEle = await this.browser.$(destinationFailedCountSpanXpath);
-    let failedCount=await desFailedCountEle.getText();
-    console.log("Failed Count : ", failedCount);
-    await saveTestData("FailedCount", failedCount);
-    await WDioHelper.takeScreenshot("EventsCount");
 }
 
 }
